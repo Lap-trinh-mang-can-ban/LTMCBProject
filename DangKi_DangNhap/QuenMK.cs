dd42b3af0a7a3a7aa0966c1fe743a49ae8a33dc0
Giang <22520358@gm.uit.edu.vn>
2024-07-02 16:34:31 +0700

    fix quen mat khau

--- DangKi_DangNhap/QuenMK.cs
+++ DangKi_DangNhap/QuenMK.cs
@@ -114,7 +114,7 @@ namespace DangKi_DangNhap
                     using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
                     {
                         smtp.EnableSsl = true;
-                        smtp.Credentials = new NetworkCredential(fromAddress, "svlo zxtg dblm nycv"); // Mật khẩu của bạn
+                        smtp.Credentials = new NetworkCredential(fromAddress, "uit1429256805c#"); // Mật khẩu của bạn
                         smtp.Send(mail);
                     }
                 }
@@ -160,7 +160,7 @@ namespace DangKi_DangNhap
                     using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
                     {
                         smtp.EnableSsl = true;
-                        smtp.Credentials = new NetworkCredential(fromAddress, "svlo zxtg dblm nycv");
+                        smtp.Credentials = new NetworkCredential(fromAddress, "uit1429256805c#");
                         smtp.Send(mail);
                     }
                 }
